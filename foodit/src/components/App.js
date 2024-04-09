46700deca08893e242e1742799e2ca7b616df1e2
seo02wow <rlatjdud9188@naver.com>
2024-04-09 14:40:34 +0900

    feat(foodit) : 최신순, 칼로리순 기능 추가

--- foodit/src/components/App.js
+++ foodit/src/components/App.js
@@ -1,10 +1,21 @@
 import FoodList from "./FoodList";
 import items from "../mock.json";
+import { useState } from "react";
 
 function App() {
   // 최상위 컴포넌트
+  const [order, setOrder] = useState("createdAt");
+  const sortedMenus = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewClick = () => setOrder("createdAt");
+  const handleCalorieClick = () => setOrder("calorie");
+
   return (
     <div>
+      <div>
+        <button onClick={handleNewClick}>최신순</button>
+        <button onClick={handleCalorieClick}>칼로리순</button>
+      </div>
       <FoodList items={items} />
     </div>
   );
