e8c3c84b02d0dae12ebfb23733573a0e017f64a0
seo02wow <rlatjdud9188@naver.com>
2024-04-09 15:54:57 +0900

    refactor : key Prop 적용

--- foodit/src/components/FoodList.js
+++ foodit/src/components/FoodList.js
@@ -17,7 +17,7 @@ function FoodList({ items, onDelete }) {
   return (
     <ul>
       {items.map((item) => (
-        <li>
+        <li key={item.id}>
           <FoodListItem item={item} onDelete={onDelete} />
         </li>
       ))}
