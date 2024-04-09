aaa107ebb6ab6041e0211736048f29c4852caf17
seo02wow <rlatjdud9188@naver.com>
2024-04-09 15:09:28 +0900

    feat : 삭제 기능 추가

--- moviepedia/src/components/App.js
+++ moviepedia/src/components/App.js
@@ -1,14 +1,20 @@
 import ReviewList from "./ReviewList";
-import items from "../mock.json";
+import mockItems from "../mock.json";
 import { useState } from "react";
 
 function App() {
   // 최상위 컴포넌트
+  const [items, setItems] = useState(mockItems);
   const [order, setOrder] = useState("createdAt");
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewstClick = () => setOrder("createdAt");
   const handleBestClick = () => setOrder("rating");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
 
   return (
     <div>
@@ -16,7 +22,7 @@ function App() {
         <button onClick={handleNewstClick}>최신순</button>
         <button onClick={handleBestClick}>베스트순</button>
       </div>
-      <ReviewList items={sortedItems} />
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
