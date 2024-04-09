6b6267314665c3ff2df6ebc2f1b76b942c2c08a4
seo02wow <rlatjdud9188@naver.com>
2024-04-09 14:28:18 +0900

    feat(moviepedia) : 베스트순, 최신순 기능 추가

--- moviepedia/src/components/App.js
+++ moviepedia/src/components/App.js
@@ -1,11 +1,21 @@
 import ReviewList from "./ReviewList";
 import items from "../mock.json";
+import { useState } from "react";
 
 function App() {
   // 최상위 컴포넌트
-  const sortedItems = items.sort((a, b) => b.rating - a.rating);
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewstClick = () => setOrder("createdAt");
+  const handleBestClick = () => setOrder("rating");
+
   return (
     <div>
+      <div>
+        <button onClick={handleNewstClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
       <ReviewList items={sortedItems} />
     </div>
   );
