2492512ddb47d7c906f58327a6586fde266da8d4
seo02wow <rlatjdud9188@naver.com>
2024-04-10 22:55:08 +0900

    feat : 리퀘스트로 데이터 가져오는 기능 추가

--- foodit/src/components/App.js
+++ foodit/src/components/App.js
@@ -1,11 +1,11 @@
 import FoodList from "./FoodList";
-import mockItems from "../mock.json";
+import { getFoods } from "./api";
 import { useState } from "react";
 
 function App() {
   // 최상위 컴포넌트
   const [order, setOrder] = useState("createdAt");
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const sortedMenus = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewClick = () => setOrder("createdAt");
@@ -16,6 +16,11 @@ function App() {
     setItems(nextItems);
   };
 
+  const handleLoadClick = async () => {
+    const { foods } = await getFoods();
+    setItems(foods);
+  };
+
   return (
     <div>
       <div>
@@ -23,6 +28,7 @@ function App() {
         <button onClick={handleCalorieClick}>칼로리순</button>
       </div>
       <FoodList items={items} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
