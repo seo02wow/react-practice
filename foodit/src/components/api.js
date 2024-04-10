13b11d90577de53450b37ff58518d2482c322388
seo02wow <rlatjdud9188@naver.com>
2024-04-10 22:59:50 +0900

    refactor(api.js) : await 키워드 추가

--- foodit/src/components/api.js
+++ foodit/src/components/api.js
@@ -1,5 +1,5 @@
 export async function getFoods() {
   const response = await fetch("https://learn.codeit.kr/9188/foods");
-  const body = response.json();
+  const body = await response.json();
   return body;
 }
