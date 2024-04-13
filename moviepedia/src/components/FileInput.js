320a56188f942e69dde7eee3b873426afe770e42
seo02wow <rlatjdud9188@naver.com>
2024-04-13 19:55:47 +0900

    feat : 파일 인풋 초기화 기능 추가

--- moviepedia/src/components/FileInput.js
+++ moviepedia/src/components/FileInput.js
@@ -1,10 +1,27 @@
+import { useRef } from "react";
+
 function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
 
-  return <input type="file" onChange={handleChange} />;
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return; // 값이 없는 경우 함수 종료
+    inputNode.value = "";
+    onChange(name, null); // 부모 컴포넌트에서 이미지 파일 값이 null
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleChange} ref={inputRef} />
+      {/* // value 값이 있을 때만 렌더링 */}
+      {value && <button onClick={handleClearClick}> X </button>}
+    </div>
+  );
 }
 
 export default FileInput;
