43f21fc14c224ca03b84a0ad2b41474bcf97a5e0
seo02wow <rlatjdud9188@naver.com>
2024-04-13 21:35:36 +0900

    feat : 이미지 미리보기 기능 추가

--- moviepedia/src/components/FileInput.js
+++ moviepedia/src/components/FileInput.js
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function FileInput({ name, value, onChange }) {
+  const [preview, setPreview] = useState();
+
   const inputRef = useRef();
 
   const handleChange = (e) => {
@@ -15,9 +17,30 @@ function FileInput({ name, value, onChange }) {
     onChange(name, null); // 부모 컴포넌트에서 이미지 파일 값이 null
   };
 
+  // value = img URL
+  // 파일을 선택할 때마다 미리 보기 주소를 바꿈
+  useEffect(() => {
+    if (!value) return;
+
+    const nextPreview = URL.createObjectURL(value); // side effect 발생
+    setPreview(nextPreview);
+
+    return () => {
+      // 정리 함수
+      setPreview(); // 빈 값으로 초기화
+      URL.revokeObjectURL(nextPreview); // ObjectURL해제
+    };
+  }, [value]);
+
   return (
     <div>
-      <input type="file" onChange={handleChange} ref={inputRef} />
+      <img src={preview} alt="이미지 미리보기" />
+      <input
+        type="file"
+        accept="image/png, image/jpeg"
+        onChange={handleChange}
+        ref={inputRef}
+      />
       {/* // value 값이 있을 때만 렌더링 */}
       {value && <button onClick={handleClearClick}> X </button>}
     </div>
