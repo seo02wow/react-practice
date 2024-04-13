c122d57f98aa30b0359c2e159076f44509afa5b2
seo02wow <rlatjdud9188@naver.com>
2024-04-13 18:26:31 +0900

    feat : file input 추가

--- moviepedia/src/components/ReviewForm.js
+++ moviepedia/src/components/ReviewForm.js
@@ -1,19 +1,25 @@
 import { useState } from "react";
 import "./ReviewForm.css";
+import FileInput from "./FileInput";
 
 function ReviewForm() {
   const [values, setValues] = useState({
     title: "",
     rating: 0,
     content: "",
+    imgFile: null,
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target; // 이벤트 객체에서 name, value 값 가져옴
+  const handleChange = (name, value) => {
     setValues((prevValues) => ({
       ...prevValues,
       [name]: value, // name 값으로 프로퍼티명 지정
     }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target; // 이벤트 객체에서 name, value 값 가져옴
+    handleChange(name, value);
   };
 
   const handleSubmit = (e) => {
@@ -23,14 +29,23 @@ function ReviewForm() {
 
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
-      <input name="title" value={values.title} onChange={handleChange} />
+      <FileInput
+        name="imgFile"
+        value={values.imgFile}
+        onChange={handleChange}
+      />
+      <input name="title" value={values.title} onChange={handleInputChange} />
       <input
         name="rating"
         type="number"
         value={values.rating}
-        onChange={handleChange}
+        onChange={handleInputChange}
       />
-      <textarea name="content" value={values.content} onChange={handleChange} />
+      <textarea
+        name="content"
+        value={values.content}
+        onChange={handleInputChange}
+      />
       <button type="submit">확인</button>
     </form>
   );
