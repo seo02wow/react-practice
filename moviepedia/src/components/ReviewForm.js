1e9abc8b43546e80f86268ecc8722515a31900d0
seo02wow <rlatjdud9188@naver.com>
2024-04-14 12:05:28 +0900

    feat : 리뷰 생성(POST) 기능 추가

--- moviepedia/src/components/ReviewForm.js
+++ moviepedia/src/components/ReviewForm.js
@@ -2,14 +2,19 @@ import { useState } from "react";
 import "./ReviewForm.css";
 import FileInput from "./FileInput";
 import Ratinginput from "./RatingInput";
+import { createReview } from "./api";
+
+const INITIAL_VALUES = {
+  title: "",
+  rating: 0,
+  content: "",
+  imgFile: null,
+};
 
 function ReviewForm() {
-  const [values, setValues] = useState({
-    title: "",
-    rating: 0,
-    content: "",
-    imgFile: null,
-  });
+  const [isSubmitting, setIsSubmitting] = useState(false); // 로딩 처리
+  const [submittingError, setSubmittingError] = useState(null); // 에러 처리
+  const [values, setValues] = useState(INITIAL_VALUES);
 
   const handleChange = (name, value) => {
     setValues((prevValues) => ({
@@ -23,9 +28,25 @@ function ReviewForm() {
     handleChange(name, value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(values);
+    const formData = new FormData();
+    formData.append("title", values.title);
+    formData.append("rating", values.rating);
+    formData.append("content", values.content);
+    formData.append("imgFile", values.imgFile);
+    try {
+      setSubmittingError(null);
+      setIsSubmitting(true); // 로딩 중일 때 버튼 비활성화
+      await createReview(formData);
+    } catch (error) {
+      setSubmittingError(error);
+      return;
+    } finally {
+      setIsSubmitting(false);
+    }
+
+    setValues(INITIAL_VALUES); // 초기화
   };
 
   return (
@@ -46,7 +67,10 @@ function ReviewForm() {
         value={values.content}
         onChange={handleInputChange}
       />
-      <button type="submit">확인</button>
+      <button type="submit" disabled={isSubmitting}>
+        확인
+      </button>
+      {submittingError?.message && <div>{submittingError.message}</div>}
     </form>
   );
 }
