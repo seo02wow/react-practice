4f30df0ebea470a187fd98acc42cd24e0610f13d
seo02wow <rlatjdud9188@naver.com>
2024-04-16 01:25:15 +0900

    refactor : Routes, Route 적용

--- codethat/src/Main.js
+++ codethat/src/Main.js
@@ -1,11 +1,22 @@
-import App from './components/App';
-import HomePage from './pages/HomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./components/App";
+import HomePage from "./pages/HomePage";
 
 function Main() {
   return (
-    <App>
-      <HomePage />
-    </App>
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="courses" element={<CourseListPage />} />
+          <Route
+            path="courses/react-frontend-development"
+            element={<CoursePage />}
+          />
+          <Route path="wishlist" element={<WishlistPage />} />
+        </Routes>
+      </App>
+    </BrowserRouter>
   );
 }
 
